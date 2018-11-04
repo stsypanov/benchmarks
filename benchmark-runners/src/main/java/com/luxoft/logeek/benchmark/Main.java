f7ee6ef6188cbbc8f9d9879d8feff9754c9f1625
stsypanov <sergei.tsypanov@yandex.ru>
2018-11-04 21:41:08 +0200

    add example for reflective call returning Boolean.TRUE

---
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/Main.java
@@ -0,0 +1,23 @@
+package com.luxoft.logeek.benchmark;
+
+import java.lang.reflect.Method;
+
+/**
+ * Пример с рефлексией: на "восьмёрке" все элементы в массиве разные
+ */
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Object[] booleans = new Object[1000];
+    Method method = Main.class.getMethod("mthd");
+
+    for (int i = 0; i < 1000; i++) {
+      booleans[i] = method.invoke(null);
+    }
+  }
+
+  public static boolean mthd() {
+    return Boolean.TRUE;
+  }
+
+}
